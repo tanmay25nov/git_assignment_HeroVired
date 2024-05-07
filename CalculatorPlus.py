36bbf415b1387306688943075df6f2cb74420127
tanmay25nov <tanmay25nov@gmail.com>
2024-05-07 21:14:52 +0530

    SQrt updated

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -21,3 +21,4 @@ print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
 # TODO: Uncomment and test the square root feature.
 num3 = 25
 print(f"The square root of {num3} = {calculator.square_root(num3)}")
+#task completed
