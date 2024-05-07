932fa6beb89b19eb5583a18305706abf26e563b0
tanmay25nov <tanmay25nov@gmail.com>
2024-05-07 21:23:45 +0530

    new changes

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -7,7 +7,9 @@ class Calculator:
     def multiply(self, a, b):
         return a * b
     def divide(self, a, b):
-        return a / b
+ 	if b == 0:
+	raise ValueError("Cannot divide by zero.")
+	return a / b
     def square_root(self, x):
         return math.sqrt(x)
 if __name__ == "__main__":
